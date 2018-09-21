c7404363c99214d806e4d5b01432dac50aa5e0c2
Liam Wright <LWright@ctds-intra.net>
2018-09-21 16:24:52 +0100

    added more bits

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,6 +9,20 @@ const client = new ApolloClient({
   uri: 'https://api-euwest.graphcms.com/v1/cjmc3gdpg13tv01cuxf2zjyzo/master'
 });
 
+const POSTS_QUERY = gql`
+{
+  posts {
+    id
+    title
+    body
+    createdAt
+  }
+}
+`;
+
+// client.query({
+//   query: testQuery
+// }).then(res => console.log(res));
 class App extends Component {
   render() {
     return (
@@ -17,9 +32,13 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
+          <Query query={POSTS_QUERY}>
+            {({ loading, data }) => {
+              if (loading) return 'Loading...';
+              const { posts } = data;
+              return posts.map(post => <h1>{post.title}</h1>);
+            }}
+          </Query>
         </div>
       </ApolloProvider>
       
